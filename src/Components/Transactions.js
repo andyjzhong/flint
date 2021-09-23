8037d6179dd54445652addeb1fc678d10dcdc8b0
andyjzhong <andyjzhong@gmail.com>
2021-09-22 21:04:58 -0400

    create add transaction button

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Button } from "@chakra-ui/react"
 import TableComponent from './Table.js'
+import './Transactions.css';
 
 const Transactions = () => {
     return (
         <div className="transactions">
             <h1>Transactions Page</h1>
-            <TableComponent />
+            <div className="transactions-table-container">
+                <Button className="btn-add-transaction" colorScheme="blue">Add Transaction</Button>
+                <TableComponent />
+            </div>
         </div>
     )
 }
