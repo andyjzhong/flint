3dd76800db89707c5b9d9c755a90017376b3c956
andyjzhong <andyjzhong@gmail.com>
2021-09-22 21:56:55 -0400

    add datepicker

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DatePickerComponent from './DatePicker.js'
 import {
     Modal,
     ModalOverlay,
@@ -10,8 +11,12 @@ import {
     FormControl,
     FormLabel,
     Input,
+    InputGroup,
+    InputLeftElement,
+    Select,
     Button
 } from "@chakra-ui/react"
+import "./Modal.css"
 
 const ModalComponent = (props) => {
 
@@ -24,13 +29,45 @@ const ModalComponent = (props) => {
                     <ModalCloseButton />
                     <ModalBody pb={6}>
                         <FormControl>
-                            <FormLabel>First name</FormLabel>
-                            <Input placeholder="First name" />
+                            <FormLabel>Description</FormLabel>
+                            <Input placeholder="i.e. Starbucks" />
                         </FormControl>
 
+                        <FormControl>
+                            <FormLabel>Category</FormLabel>
+                            <Select placeholder="Select category">
+                                <option value="option1">Option 1</option>
+                                <option value="option2">Option 2</option>
+                                <option value="option3">Option 3</option>
+                            </Select>
+                        </FormControl>
+
+                        <FormControl>
+                            <FormLabel>Subcategory</FormLabel>
+                            <Select placeholder="Select category">
+                                <option value="option1">Option 1</option>
+                                <option value="option2">Option 2</option>
+                                <option value="option3">Option 3</option>
+                            </Select>
+                        </FormControl>
+
+                        <FormControl>
+                            <FormLabel>Date</FormLabel>
+                            <DatePickerComponent className="transaction-date-picker"/>
+                        </FormControl>
+
+
                         <FormControl mt={4}>
-                            <FormLabel>Last name</FormLabel>
-                            <Input placeholder="Last name" />
+                            <FormLabel>Amount</FormLabel>
+                            <InputGroup>
+                                <InputLeftElement
+                                    pointerEvents="none"
+                                    color="gray.300"
+                                    fontSize="1.2em"
+                                    children="$"
+                                />
+                                <Input placeholder="Enter amount" />
+                            </InputGroup>
                         </FormControl>
                     </ModalBody>
 
