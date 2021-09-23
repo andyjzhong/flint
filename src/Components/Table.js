223d2415981917cd7223643010ec9cfaf9336683
andyjzhong <andyjzhong@gmail.com>
2021-09-23 19:52:11 -0400

    add buttons to dummy table

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Table, Thead, Tr, Th, Td, Tbody } from '@chakra-ui/react';
+import { Table, Thead, Tr, Th, Td, Tbody, Button } from '@chakra-ui/react';
 import './Table.css';
 import axios from 'axios';
 
@@ -37,6 +37,8 @@ const TableComponent = () => {
                         <Th>Category</Th>
                         <Th>Subcategory</Th>
                         <Th isNumeric>Amount</Th>
+                        <Th>Edit</Th>
+                        <Th>Delete</Th>
                     </Tr>
                 </Thead>
                 <Tbody>
@@ -47,6 +49,8 @@ const TableComponent = () => {
                         <Td>Food</Td>
                         <Td>Coffee Shops</Td>
                         <Td isNumeric>8.25</Td>
+                        <Td><Button className="btn-edit-transaction" colorScheme="orange">Edit</Button></Td>
+                        <Td><Button className="btn-edit-transaction" colorScheme="red">Delete</Button></Td>
                     </Tr>
                     <Tr>
                         <Td>McDonalds</Td>
@@ -55,6 +59,8 @@ const TableComponent = () => {
                         <Td>Food</Td>
                         <Td>Fast Food</Td>
                         <Td isNumeric>14.75</Td>
+                        <Td><Button className="btn-edit-transaction" colorScheme="orange">Edit</Button></Td>
+                        <Td><Button className="btn-edit-transaction" colorScheme="red">Delete</Button></Td>
                     </Tr>
                     <Tr>
                         <Td>Wal-Mart</Td>
@@ -63,6 +69,8 @@ const TableComponent = () => {
                         <Td>Food</Td>
                         <Td>Groceries</Td>
                         <Td isNumeric>38.33</Td>
+                        <Td><Button className="btn-edit-transaction" colorScheme="orange">Edit</Button></Td>
+                        <Td><Button className="btn-edit-transaction" colorScheme="red">Delete</Button></Td>
                     </Tr>
                 </Tbody>
             </Table>
--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button } from "@chakra-ui/react"
 import TableComponent from './Table.js'
 import ModalComponent from './Modal.js'
@@ -9,11 +9,19 @@ const Transactions = () => {
 
     const { isOpen, onOpen, onClose } = useDisclosure()
 
+    useEffect(() => {
+        getUser()
+}, [])
+
+    const getUser = () => {
+        console.log("Inside getUser");
+    }
+
     return (
         <div className="transactions">
             <h1>Transactions Page</h1>
             <div className="transactions-table-container">
-                <Button className="btn-add-transaction" colorScheme="blue" onClick={onOpen}>
+                <Button className="btn-add-transaction" colorScheme="green" onClick={onOpen}>
                     Add Transaction
                 </Button>
                 <TableComponent />
