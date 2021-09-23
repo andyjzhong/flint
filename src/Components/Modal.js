d026b3c27fbec741fba2cc1af9917dfd4e367623
andyjzhong <andyjzhong@gmail.com>
2021-09-22 21:31:48 -0400

    add modal

---
+++ src/Components/Modal.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    FormControl,
+    FormLabel,
+    Input,
+    Button
+} from "@chakra-ui/react"
+
+const ModalComponent = (props) => {
+
+    return (
+        <>
+            <Modal isOpen={props.isOpen} onClose={props.onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Create your account</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <FormControl>
+                            <FormLabel>First name</FormLabel>
+                            <Input placeholder="First name" />
+                        </FormControl>
+
+                        <FormControl mt={4}>
+                            <FormLabel>Last name</FormLabel>
+                            <Input placeholder="Last name" />
+                        </FormControl>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={props.onClose}>Save</Button>
+                        <Button onClick={props.onClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
+}
+
+export default ModalComponent;
--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Button } from "@chakra-ui/react"
 import TableComponent from './Table.js'
+import ModalComponent from './Modal.js'
+import { useDisclosure } from "@chakra-ui/react"
 import './Transactions.css';
 
 const Transactions = () => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
         <div className="transactions">
             <h1>Transactions Page</h1>
             <div className="transactions-table-container">
-                <Button className="btn-add-transaction" colorScheme="blue">Add Transaction</Button>
+                <Button className="btn-add-transaction" colorScheme="blue" onClick={onOpen}>
+                    Add Transaction
+                </Button>
                 <TableComponent />
+                <ModalComponent isOpen={isOpen} onOpen={onOpen} onClose={onClose}/>
             </div>
         </div>
     )
