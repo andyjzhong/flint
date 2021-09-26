ddaf35256783d184fc92910c5d61fdaeea47219b
jexxer <jexxe@live.com>
2021-09-26 01:14:09 -0400

    persistent login for budgets

--- src/Components/Budgets.js
+++ src/Components/Budgets.js
@@ -1,42 +1,72 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import { Button } from "@chakra-ui/react"
+import axios from 'axios';
+import jwt_decode from 'jwt-decode'
 import BudgetTableComponent from './BudgetTable.js'
 import CreateBudgetModal from './CreateBudgetModal.js'
 import EditBudgetModal from './EditBudgetModal.js'
 import { useDisclosure } from "@chakra-ui/react"
 import BudgetChart from './BudgetChart.js';
+import { DataContext } from './DataContext';
 import './Transactions.css';
 
 const Budgets = () => {
-
+    let history = useHistory()
+    const { accessToken, setAccessToken } = useContext(DataContext);
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const [isTokenValid, setIsTokenValid] = useState(false)
 
-    useEffect(() => {
-        getUser()
-}, [])
+    const refreshToken = async () => {
+        try{
+            const decoded = jwt_decode(localStorage.getItem('refreshToken'))
 
-    const getUser = () => {
-        console.log("Inside getUser");
+            const res = await axios.post('http://localhost:8000/users/refreshtoken', {
+                email: decoded.email,
+                token: localStorage.getItem('refreshToken')
+            }).catch((err) => {
+                console.log(err)
+            })
+
+            localStorage.setItem('refreshToken', res.data.refreshToken)
+            setIsTokenValid(true)
+            setAccessToken(res.data.accessToken)
+        } catch {
+            history.push('/login')
+        }
     }
 
-    return (
-        <div className="transactions">
-            <h1>Budgets Page</h1>
-            <div class="page-container" style={{display: 'flex'}}>
-                <div class="chart-container" style={{margin: '1rem', border: '1px solid lightgray', padding: '1rem', borderRadius: '1rem'}}>
-                    <BudgetChart />
-                </div>
-                <div className="transactions-table-container">
-                    <Button className="btn-add-transaction" colorScheme="green" onClick={onOpen}>
-                        Add Budgets
-                    </Button>
-                    <BudgetTableComponent />
-                    <CreateBudgetModal isOpen={isOpen} onOpen={onOpen} onClose={onClose}/>
-                    <EditBudgetModal isOpen={isOpen} onOpen={onOpen} onClose={onClose}/>
+    useEffect(() => {
+        refreshToken()
+    },[])
+
+    if(isTokenValid){
+        return (
+            <div className="transactions">
+                <h1>Budgets Page</h1>
+                <div class="page-container" style={{display: 'flex'}}>
+                    <div class="chart-container" style={{margin: '1rem', border: '1px solid lightgray', padding: '1rem', borderRadius: '1rem'}}>
+                        <BudgetChart />
+                    </div>
+                    <div className="transactions-table-container">
+                        <Button className="btn-add-transaction" colorScheme="green" onClick={onOpen}>
+                            Add Budgets
+                        </Button>
+                        <BudgetTableComponent />
+                        <CreateBudgetModal isOpen={isOpen} onOpen={onOpen} onClose={onClose}/>
+                        <EditBudgetModal isOpen={isOpen} onOpen={onOpen} onClose={onClose}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="transactions">
+                <h1>Budgets Page</h1>
+                
+            </div>
+        )
+    }
 }
 
 export default Budgets;
