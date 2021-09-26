1bcd05f73ee3b2cf532645e300fb0348e1035370
andyjzhong <andyjzhong@gmail.com>
2021-09-26 18:22:23 -0400

    refactor comp chart data

--- src/Components/ComparisonChart.js
+++ src/Components/ComparisonChart.js
@@ -2,39 +2,6 @@ import React, { useContext } from 'react';
 import { DataContext } from './DataContext';
 import { Bar } from 'react-chartjs-2';
 
-const data = {
-  labels: ['1', '2', '3', '4', '5', '6'],
-  datasets: [
-    {
-      label: '# of Red Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: 'rgb(255, 99, 132)',
-    },
-    {
-      label: '# of Blue Votes',
-      data: [2, 3, 20, 5, 1, 4],
-      backgroundColor: 'rgb(54, 162, 235)',
-    },
-    {
-      label: '# of Green Votes',
-      data: [3, 10, 13, 15, 22, 30],
-      backgroundColor: 'rgb(75, 192, 192)',
-    },
-  ],
-};
-
-const options = {
-  scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true,
-        },
-      },
-    ],
-  },
-};
-
 function ComparisonChart() {
 
     const { summaryData } = useContext(DataContext);
@@ -53,6 +20,39 @@ function ComparisonChart() {
 
         console.log("Total spend by category: ", doughnutBudgetValues);
     }
+
+    const data = {
+        labels: ['1', '2', '3', '4', '5', '6'],
+        datasets: [
+            {
+              label: '# of Red Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: 'rgb(255, 99, 132)',
+            },
+            {
+              label: '# of Blue Votes',
+              data: [2, 3, 20, 5, 1, 4],
+              backgroundColor: 'rgb(54, 162, 235)',
+            },
+            {
+              label: '# of Green Votes',
+              data: [3, 10, 13, 15, 22, 30],
+              backgroundColor: 'rgb(75, 192, 192)',
+            },
+        ],
+    };
+
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
+    };
 
     return (
         <>
