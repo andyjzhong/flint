1dd24a44563053346930f7cebe4525efd22de964
jexxer <jexxe@live.com>
2021-09-28 19:05:29 -0400

    account settings half done

---
+++ src/Components/AccountPage/ChangeEmailModal.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Input,
+    FormControl,
+    FormLabel,
+    Button
+  } from "@chakra-ui/react"
+  import axios from 'axios';
+
+function ChangeEmailModal(props) {
+    const userId = localStorage.getItem('fuid')
+
+    const [email, setEmail] = useState(props.userInfo.email)
+  
+    async function submitEmailChange(){
+      
+      const url =
+              process.env.NODE_ENV === 'production'
+                  ? `http://flint-server.herokuapp.com/users/changeemail/${userId}`
+                  : `http://localhost:8000/users/changeemail/${userId}`
+      const res = axios.put(url, {
+        email: email,
+      },{
+        headers: {'authorization': `Bearer ${props.accessToken}`}
+      })
+        .then((user) => {
+          console.log('updated user:', user)
+          props.setUserInfo(user.data)
+        })
+    }
+  
+  
+      return (
+          <>
+            <Modal
+              isOpen={props.isEmailChangeOpen}
+              onClose={props.onEmailClose}
+            >
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Edit</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody pb={6}>
+                  <FormControl>
+                    <FormLabel>Email</FormLabel>
+                    <Input 
+                      placeholder="First name" 
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </FormControl>
+                </ModalBody>
+      
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} onClick={() => {
+                    submitEmailChange()
+                    props.onEmailChangeClose()
+                  }}>
+                    Save
+                  </Button>
+                  <Button onClick={props.onEmailChangeClose}>
+                    Cancel
+                  </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </>
+        )
+}
+
+export default ChangeEmailModal;
---
+++ src/Components/AccountPage/ChangeNameModal.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Input,
+    FormControl,
+    FormLabel,
+    Button
+  } from "@chakra-ui/react"
+  import axios from 'axios';
+
+function ChangeNameModal(props) {
+
+  const userId = localStorage.getItem('fuid')
+
+  const [firstName, setFirstName] = useState(props.userInfo.firstName)
+  const [lastName, setLastName] = useState(props.userInfo.lastName)
+
+  async function submitNameChange(){
+    
+    const url =
+            process.env.NODE_ENV === 'production'
+                ? `http://flint-server.herokuapp.com/users/changename/${userId}`
+                : `http://localhost:8000/users/changename/${userId}`
+    const res = axios.put(url, {
+      firstName: firstName,
+      lastName: lastName
+    },{
+      headers: {'authorization': `Bearer ${props.accessToken}`}
+    })
+      .then((user) => {
+        console.log('updated user:', user)
+        props.setUserInfo(user.data)
+      })
+  }
+
+
+    return (
+        <>
+          <Modal
+            isOpen={props.isNameChangeOpen}
+            onClose={props.onNameChangeClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Edit</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody pb={6}>
+                <FormControl>
+                  <FormLabel>First name</FormLabel>
+                  <Input 
+                    placeholder="First name" 
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </FormControl>
+    
+                <FormControl mt={4}>
+                  <FormLabel>Last name</FormLabel>
+                  <Input 
+                    placeholder="Last name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </FormControl>
+              </ModalBody>
+    
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3} onClick={() => {
+                  submitNameChange()
+                  props.onNameChangeClose()
+                }}>
+                  Save
+                </Button>
+                <Button onClick={props.onNameChangeClose}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
+      )
+}
+
+export default ChangeNameModal;
