99dedb8864e907b0e98bcff079558b11e4d7c1f3
jexxer <jexxe@live.com>
2021-10-01 10:45:50 -0400

    changed about page

---
+++ src/Components/AboutPage/AboutPage.jsx
@@ -0,0 +1,69 @@
+import { Box, Button, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
+import * as React from 'react';
+import { CardWithAvatar } from './CardWithAvatar';
+import data from './data.json';
+import { FollowerCount } from './FollowerCount';
+import { UserInfo } from './UserInfo';
+
+function AboutPage() {
+  return (
+    <Box
+      bg={useColorModeValue('gray.100', 'gray.800')}
+      px={{
+        base: '6',
+        md: '8',
+      }}
+      py="12"
+    >
+      <Box
+        as="section"
+        maxW={{
+          base: 'xs',
+          md: '3xl',
+        }}
+        mx="auto"
+      >
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+          }}
+          spacing="6"
+        >
+          {data.map(user => {
+            const { name, bio, src, isVerified, followerCount } = user;
+            return (
+              <CardWithAvatar
+                maxW='72'
+                key={name}
+                avatarProps={{
+                  src,
+                  name,
+                }}
+              >
+                <UserInfo
+                  mt="3"
+                  name={name}
+                  bio={bio}
+                  isVerified={isVerified}
+                />
+                <FollowerCount my="4" count={followerCount} />
+                <Button
+                  variant="outline"
+                  colorScheme="blue"
+                  rounded="full"
+                  size="sm"
+                  width="full"
+                >
+                  View Profile
+                </Button>
+              </CardWithAvatar>
+            );
+          })}
+        </SimpleGrid>
+      </Box>
+    </Box>
+  );
+}
+
+export default AboutPage;
